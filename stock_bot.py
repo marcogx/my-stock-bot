a23b9c075abce55ee09b51c4f3e70685cade6793
marcogx <gx239@nyu.edu>
2017-09-19 18:48:11 -0700

    start bonfire

---
+++ stock_bot.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+buying_power = 35000
+
+long_term_hold = ['HTHT', 'BABA']
+
+chinese_market_prof = ['BABA', 'HTHT', 'FB']
+ecommerce_prof = ['BABA']
+medical_prof = ['VEEV', 'MZOR', 'ALIGN']
+robotics_prof = ['MZOR', 'CGNX', 'IRBT', 'TSLA']
+social_network_prof = ['FB']
+networking_solution_prof = ['ANET']
+electric_auto_driving_prof = ['TSLA', 'NVDA', 'CGNX']
+ai_prof = ['TSLA', 'NVDA']
+cloud_platform_software = ['APPF', 'VEEV']
+consumer_daily_life_prof = ['APPF', 'HTHT', 'IRBT', 'NVDA'] # 衣食住行玩
